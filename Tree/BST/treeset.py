826755b7d7bbfb7cca6494a9f8bd1d955e09495f
Heli Wang <h379wang@uwaterloo.ca>
2019-09-08 18:23:27 -0400

    bisect implementation of treeset

--- Tree/BST/treeset.py
+++ Tree/BST/treeset.py
@@ -1,5 +1,44 @@
 """
 TreeSet Implementation using binary search (bisect)
+
+Usage:
+from treeset import TreeSet
+ts = TreeSet([3,7,2,7,1,3])
+print(ts)
+>>> [1, 2, 3, 7]
+
+ts.add(4)
+print(ts)
+>>> [1, 2, 3, 4, 7]
+
+ts.remove(7)
+print(ts)
+>>> [1, 2, 3, 4]
+
+ts.remove(5)
+print(ts)
+>>> [1, 2, 3, 4]
+
+ts.addAll([3,4,5,6])
+print(ts)
+>>> [1, 2, 3, 4, 5, 6]
+
+print(ts[0])
+>>> 1
+
+print(ts[-1])
+>>> 6
+
+print(1 in ts)
+>>> True
+
+print(100 in ts)
+>>> False
+
+for i in TreeSet([1,3,1]):
+	print(i)
+>>> 1
+>>> 3
 """
 import bisect
 
