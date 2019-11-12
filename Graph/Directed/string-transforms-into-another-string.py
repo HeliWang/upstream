7660157127fcbbb67f983f809a2ecaf01cf32b86
Heli Wang <h379wang@uwaterloo.ca>
2019-11-11 23:09:18 -0500

    dp/graph

--- Graph/Directed/string-transforms-into-another-string.py
+++ Graph/Directed/string-transforms-into-another-string.py
@@ -14,6 +14,32 @@ class Solution(object):
         for example, the following will not be valid
         "abcdefghijklmnopqrstuvwxyz"
         "bcdefghijklmnopqrstuvwxyza"
+        should be False
+
+        "abcdefghijklmnopqrstuvwxyz"
+        "abcdefghijklmnopqrstuvwxyz"
+        should be True
+
+        "abcdefghijklmnopqrstuvw"
+        "bcdefghijklmnopqrstuvwx"
+        should be True
+
+        Model the problem as a graph problem.
+        Add an edge from one character to another if you need to convert between them.
+        What if one character needs to be converted into more than one character?
+        There would be no solution. Thus, every node can have at most one outgoing edge.
+        How to process a cycle?
+
+        It is not possible to convert x directly to y when y is also in str1
+        however, if there is any spare char not used in str1, then we can convert x to z, then z to y
+        thus, only when len(str1) == 26, then there is no spare char in str1.
+
+        However, even len(str1) == 26, we may be able to convert:
+          two distinct chars in str1 convert to the same char in str2
+          first convert the two dist chars to the same char in str2, then there is a spare char available in str1
+
+        However, we also need to consider the corner case:
+          all char in str1 convert to the same char to str2
         """
         n = len(str1)
         if n != len(str2):
